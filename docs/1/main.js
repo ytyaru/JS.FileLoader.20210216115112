c80b247162b2c6c7509d05f42be6edb06b5955ad
ytyaru <yttry0@gmail.com>
2021-02-16 13:11:48 +0900

    ES Module

---
+++ docs/1/main.js
@@ -0,0 +1,11 @@
+import FileLoader from './FileLoader.js';
+
+window.addEventListener('load', async(event) => {
+    const text = await FileLoader.text('./contents/some.txt');
+    console.log(text);
+    const json = await FileLoader.json('./contents/some.json');
+    console.log(json);
+
+    console.log(await FileLoader.load('./contents/some.txt'));
+    console.log(await FileLoader.load('./contents/some.json'));
+});
